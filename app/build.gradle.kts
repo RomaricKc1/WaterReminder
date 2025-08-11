f1675439ee30d3a8521ebed3c64d7ac8ebed7a2c
RomaricKc1 <45500198+RomaricKc1@users.noreply.github.com>
2025-08-11 22:52:28 +0200

    bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.romarickc.reminder"
         minSdk = 30
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         vectorDrawables.useSupportLibrary = true
     }
 
